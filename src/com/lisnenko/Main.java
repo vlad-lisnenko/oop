e57c599bb47fe8f353768fc78acb373968eaeebb
vlad lisnenko <lisnenkov777@gmail.com>
2021-04-27 15:23:01 +0300

    some changes in the class Main

--- src/com/lisnenko/Main.java
+++ src/com/lisnenko/Main.java
@@ -6,16 +6,16 @@ import com.lisnenko.shape.Square;
 import com.lisnenko.shape.Triangle;
 
 import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle(23.4);
         Square square = new Square(12.0);
         Triangle triangle = new Triangle(2, 3, 4);
-        System.out.println(circle);
-        System.out.println(square);
-        System.out.println(triangle);
+        ArrayList<Shape> shapes = new ArrayList<>();
+        shapes.add(circle);
+        shapes.add(square);
+        shapes.add(triangle);
+        shapes.stream().forEach(shape -> System.out.println(shape.getPerimeter() + shape.getSquare()));
     }
 }
